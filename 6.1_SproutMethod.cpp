749ce0c719951b5567ab1a10b01a9b72025d6e33
JunichiSato <sato440nomusuko@gmail.com>
2017-12-11 22:01:47 +0900

    p68

--- 6.1_SproutMethod.cpp
+++ 6.1_SproutMethod.cpp
@@ -1,10 +1,14 @@
 public class TransactionGate
 {
     public void postEntries(List entries) {
+        List entriesToAdd = new LinkedList();
         for (Iterator it = entries.iterator(); it.hasNext(); ) {
             Entry entry = (Entry)it.next();
-            entry.postDate();
+            if (!transactionBundle.getListManager().hasEntry(entry)) {
+                entry.postDate();
+                entriesToAdd.add(entry);
+            }
         }
-        transactionBundle.getListManager().add(entries);
+        transactionBundle.getListManager().add(entriesToAdd);
     }
 }
