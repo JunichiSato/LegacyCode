901481556c092d6f76a4ad2c36c6d2ea439417fb
JunichiSato <sato440nomusuko@gmail.com>
2017-12-11 22:25:45 +0900

    p69 スプラウトメソッド終わり

--- 6.1_SproutMethod.cpp
+++ 6.1_SproutMethod.cpp
@@ -11,4 +11,15 @@ public class TransactionGate
         }
         transactionBundle.getListManager().add(entriesToAdd);
     }
+
+    public void uniqueEntries(List entries) {
+        List result = new ArrayList();
+        for (Iterator it = new entries.iterator(); it.hasNext(); ) {
+            Entry entry = (Entry)it.next();
+            if (!transactionBundle.getListManager().hasEntry(entry)) {
+                result.add(entry);
+            }
+        }
+        return result;
+    }
 }
